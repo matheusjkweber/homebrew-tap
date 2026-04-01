08e42d7dea8825b463a451d76ae51c376b4c92d2
Matheus Weber <15331222+matheusjkweber@users.noreply.github.com>
2026-04-01 20:27:35 -0300

    Update cosmoremote to 1.0.1

---
+++ Formula/cosmoremote.rb
@@ -0,0 +1,27 @@
+class Cosmoremote < Formula
+  desc "Control Claude Code and Codex on your Mac from your phone"
+  homepage "https://github.com/matheusjkweber/cosmoremote"
+  url "https://registry.npmjs.org/cosmoremote/-/cosmoremote-1.0.1.tgz"
+  sha256 "698028ce8a86b0c8d729ec7a524671cb4af6ab0d52f219d77aa5da49d830547c"
+  license "MIT"
+
+  depends_on "node"
+
+  def install
+    system "npm", "install", *std_npm_args
+    bin.install_symlink Dir["#{libexec}/bin/*"]
+  end
+
+  def caveats
+    <<~EOS
+      To start the bridge daemon, run:
+        cosmoremote start
+
+      Then scan the QR code with the CosmoRemote app on your phone.
+    EOS
+  end
+
+  test do
+    assert_match "cosmoremote", shell_output("#{bin}/cosmoremote --help")
+  end
+end
